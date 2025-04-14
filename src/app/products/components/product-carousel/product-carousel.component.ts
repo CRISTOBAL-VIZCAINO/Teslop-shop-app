80a009278229ed46473888a5c219a4f014687bd6
Cristóbal Vizcaíno M <cristobaljulian123456@gmail.com>
2025-04-14 14:46:57 -0500

    Fronet_Teslo_Shop

---
+++ src/app/products/components/product-carousel/product-carousel.component.ts
@@ -0,0 +1,75 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  input,
+  OnChanges,
+  SimpleChanges,
+  viewChild,
+} from '@angular/core';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { ProductImagePipe } from '@products/pipes/product-image.pipe';
+
+@Component({
+  selector: 'product-carousel',
+  imports: [ProductImagePipe],
+  templateUrl: './product-carousel.component.html',
+  styles: `
+    .swiper {
+      width: 100%;
+      height: 500px;
+    }
+
+
+  `,
+})
+export class ProductCarouselComponent implements AfterViewInit, OnChanges {
+  images = input.required<string[]>();
+  swiperDiv = viewChild.required<ElementRef>('swiperDiv');
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['images']) {
+      this.swiperInit();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.swiperInit();
+  }
+
+  swiperInit(){
+    const element = this.swiperDiv().nativeElement;
+    if (!element) return;
+
+    const swiper = new Swiper(element, {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+
+      modules: [Navigation, Pagination],
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  baseUrl: 'https://angular-teslop-shop.onrender.com/api',
+};
